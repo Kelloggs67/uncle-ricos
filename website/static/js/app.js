1d3d424ac1f0c1e5d114b07bd627176df9e381ce
Kelloggs67 <timkhutch@gmail.com>
2021-02-22 04:11:13 -0500

    smooth scrolling and sections

--- website/static/js/app.js
+++ website/static/js/app.js
@@ -2,7 +2,7 @@ const navSlide = () => {
   const burger = document.querySelector(".burger img");
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
-  const cancel = document.querySelector(".cancel")
+  const cancel = document.querySelector(".cancel");
 
   burger.addEventListener("click", () => {
     //Toggle Nav
@@ -12,18 +12,31 @@ const navSlide = () => {
 
     //Animate Links
     navLinks.forEach((link, index) => {
-      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.6}s`;
-
+      link.style.animation = `navLinkFade 0.5s ease forwards ${
+        index / 7 + 0.6
+      }s`;
     });
-    //Toggle Burger
   });
+  //Toggle Burger
   cancel.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
     burger.classList.toggle("burger-clicked");
     cancel.classList.toggle("cancel-display");
 
-    navLinks.forEach((link, index) => {
+    navLinks.forEach((link) => {
       link.style.animation = "";
+    });
+  });
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      nav.classList.toggle("nav-active");
+      burger.classList.toggle("burger-clicked");
+      cancel.classList.toggle("cancel-display");
+
+      navLinks.forEach((link) => {
+        link.style.animation = "";
+      });
     });
   });
 };
